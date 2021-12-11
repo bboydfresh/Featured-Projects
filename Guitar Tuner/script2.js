66217b2681c786432bf25c8fe1d85127db820602
bboydfresh <69738088+bboydfresh@users.noreply.github.com>
2021-12-11 17:43:49 -0500

    Add files via upload

---
+++ Guitar Tuner/script2.js
@@ -0,0 +1,25 @@
+const sounds = ['Low E Flat', 'A Flat', 'D Flat', 'G Flat', 'B Flat', 'High E Flat']
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+
+    btn.innerText = sound
+
+    btn.addEventListener('click', () => {
+        stopSongs()
+
+        document.getElementById(sound).play()
+    })
+
+    document.getElementById('buttons').appendChild(btn)
+})
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+
+        song.pause()
+        song.currentTime = 0;
+    })
+}
